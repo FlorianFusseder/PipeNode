13d26f71f5c03800ca7267b33f96f95a0130d795
Florian-Desktop <florianfueder@yahoo.de>
2017-06-16 15:50:14 +0200

    added scripts, renamed and reworked node

--- source.cpp
+++ source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 
+// #include "opencv2/opencv.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/image.hpp"
@@ -11,31 +12,28 @@ class BridgeNode: public rclcpp::node::Node
         BridgeNode() : Node("BridgeNode", true)
         {
             auto qos = rmw_qos_profile_sensor_data;
+            auto pubStr = "image";
+            auto subStr = "watermarked_image";
             publisher = this->create_publisher<sensor_msgs::msg::Image>
-                ("/results/segmentation", qos);
-            std::weak_ptr<std::remove_pointer<decltype(publisher.get())>::type>
-                captured_publisher = publisher;
+                (subStr, qos);
+            captured_publisher = publisher;
             subscribtion = this->create_subscription<sensor_msgs::msg::Image> (
-                "/image", 
-                std::bind(&BridgeNode::Sub_Callback, this, std::placeholders::_1)
-                , qos);
+                pubStr, 
+                std::bind(&BridgeNode::Sub_Callback, this, std::placeholders::_1),
+                qos);
         } 
     private:
-        void Sub_Callback(sensor_msgs::msg::Image::SharedPtr msg)
+        void Sub_Callback(sensor_msgs::msg::Image::UniquePtr msg)
         {
-            std::cout << "I got a picture" << std::endl;
-            // Pub_Callback(msg);
-        }
-
-        void Pub_Callback(sensor_msgs::msg::Image::SharedPtr msg)
-        {
-            msg->is_bigendian = false;
-            std::cout <<"I publish: " << std::endl;
-            // publisher->publish(msg);
+            auto p = captured_publisher.lock();
+            if(!p)
+                return;
+            p->publish(msg);
         }
 
         rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscribtion;
         rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher;
+        std::weak_ptr<std::remove_pointer<decltype(publisher.get())>::type> captured_publisher;
 };
 
 int main(int argc, char* argv[]){
